cf3322e1b98b7fae6854022d300b17018dabba43
Da Ma <mad29@pc-cheshmi-04.ece.mcmaster.ca>
2024-11-13 21:20:09 -0500

    initial code structures setup

--- mlir/include/mlir/Dialect/LinearSolver/LinearSolverOps.h
+++ mlir/include/mlir/Dialect/LinearSolver/LinearSolverOps.h
@@ -1,1 +1,16 @@
-// Header for operations in the dialect
+//===- LinearSolverOps.h - LinearSolver operations ---------------*- C++ -*-===//
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_LINEARSOLVER_LINEARSOLVEROPS_H
+#define MLIR_DIALECT_LINEARSOLVER_LINEARSOLVEROPS_H
+
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+#define GET_OP_CLASSES
+#include "mlir/Dialect/LinearSolver/LinearSolverOps.h.inc"
+
+#endif // MLIR_DIALECT_LINEARSOLVER_LINEARSOLVEROPS_H
--- mlir/lib/Dialect/LinearSolver/LinearSolverOps.cpp
+++ mlir/lib/Dialect/LinearSolver/LinearSolverOps.cpp
@@ -1,1 +1,30 @@
-// Implementation of operations
+//===- LinearSolverOps.cpp - Implementation of LinearSolver operations ----===//
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/LinearSolver/LinearSolverOps.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/OpImplementation.h"
+
+using namespace mlir;
+using namespace mlir::linear_solver;
+
+#define GET_OP_CLASSES
+#include "mlir/Dialect/LinearSolver/LinearSolverOps.cpp.inc"
+
+// TODO: Placeholder verifications to be implemented
+
+LogicalResult TriangularSolveOp::verify() {
+  // Ensure that the matrix is square
+  // Ensure the RHS and solution have compatible shapes
+  if (/* Check matrix dimensions here */)
+    return emitOpError("expected a square matrix for triangular solve");
+
+  return success();
+}
+
+LogicalResult LUFactorizeOp::verify() { return success(); }
+LogicalResult ILUFactorizeOp::verify() { return success(); }
+LogicalResult CholeskyDecomposeOp::verify() { return success(); }
+LogicalResult ForwardSubstitutionOp::verify() { return success(); }
+LogicalResult BackwardSubstitutionOp::verify() { return success(); }
